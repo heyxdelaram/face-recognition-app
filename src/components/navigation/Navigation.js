78dd475e51c735a6d8c561a0fdb323249414ad9d
heyxdelaram <dellafarsa@gmail.com>
2023-04-03 18:01:09 +0430

    initial configs

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+    return(
+        <nav className="">
+            <img src="" alt=""/>
+            <p>Face Recognition</p>
+            <p>Sign up</p>
+        </nav>
+    )
+}
+
+export default Navigation;
