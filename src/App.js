284b641b49a29d5a463d88203b41d67247ee1321
heyxdelaram <dellafarsa@gmail.com>
2023-04-04 20:37:12 +0430

    InputURL added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import './App.css'
+import './App.css';
 import ParticlesBg from "particles-bg";
 import Navigation from "./components/navigation/Navigation";
+import InputURL from "./components/inputURL/InputURL";
 
 function App() {
   return (
     <div className="App">
     <ParticlesBg color="#f1f1f1" num={150} type="cobweb" bg={true} />
       <Navigation />
-      {/* {<Rank />
       <InputURL />
+      {/* {<Rank />
+      
       <FaceRecognition />} */}
     </div>
   );
---
+++ src/components/inputURL/InputURL.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './InputURL.css';
+
+const InputURL = () => {
+    return (
+        <section className="d-flex justify-content-center">
+            <div className="p-5 shadow-5 br4 bg-white">
+                <input type="text" placeholder="url" className="m-1 me-2 br4 p-2"/>
+                <button className="detect-btn m-1 br4 grow">Detect</button>
+            </div>
+        </section>
+    )
+}
+
+export default InputURL;
